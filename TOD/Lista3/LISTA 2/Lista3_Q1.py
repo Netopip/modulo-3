8109fd5d695b859aa6e35814d951b11187b88c92
Neto <fernandesneto1999@gmail.com>
2025-05-09 15:20:10 -0300

    novos exercicios

--- TOD/Lista3/LISTA 2/Lista3_Q1.py
+++ TOD/Lista3/LISTA 2/Lista3_Q1.py
@@ -1,8 +1,37 @@
 from random import *
 
-def listaComCemElementos():
+def listaElementos(listaNumeros:list):
+    lista_par = []
+    lista_impar = []
+    cont_par = 0
+    cont_impar = 0
     
-    elementos = [randint(-100,100) for i in range(100)]
-    return elementos
+    lista = listaNumeros
+    for i in lista:
+        if type(i) != int:
+            return Exception
+        else:
+            if i%2 == 0:
+                lista_par.append(i)
+                cont_par +=1
+            else:
+                lista_impar.append(i)
+                cont_impar += 1
+    
+    return lista_par,lista_impar,cont_par,cont_impar
 
-print(listaComCemElementos())
+assert listaElementos([1,2,3,4,5]) == ([2,4],[1,3,5],2,3)
+assert listaElementos([0,1,2,3,4,5,6,7,8,9,10]) == ([0,2,4,6,8,10],[1,3,5,7,9],6,5)
+assert listaElementos([1,2,'a',4,5,6]) == Exception
+assert listaElementos(['',2,3,4,5,6]) == Exception
+assert listaElementos([1,2,3.5,4,5,6]) == Exception
+
+print('testes OK')
+            
+    
+
+
+        
+    
+    
+
