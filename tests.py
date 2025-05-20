fa2af484225f4331c763500997a9613091f13f43
Neto <fernandesneto1999@gmail.com>
2025-05-19 21:27:22 -0300

    novo arquivo

---
+++ tests.py
@@ -0,0 +1,20 @@
+
+
+def NumeroPar(n):
+    if type(n)!= int:
+        return Exception
+    else:
+        return True if n%2 == 0 else False
+
+
+
+if __name__ == '__main__':
+    assert NumeroPar(2) == True
+    assert NumeroPar(3) == False
+    assert NumeroPar(0) == True
+    assert NumeroPar(-2) == True
+    assert NumeroPar('a') == Exception
+    assert NumeroPar(2.5) == Exception
+    
+print('Te"stes ok')
+    
